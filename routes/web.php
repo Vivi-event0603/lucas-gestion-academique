9887088576f677a80f29cf10b3a03bbdfb831d31
Vivi-event0603 <vdepoukn@gmail.com>
2026-02-07 21:48:23 +0100

    Fix duplicate StudentController import in routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,6 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\MemoireController;
 
 /*
 |--------------------------------------------------------------------------
